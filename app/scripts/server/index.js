a79af63744b936ddcb7d08714de869884e560870
Bovtik <bovtik.work@gmail.com>
2016-08-27 02:16:20 +0300

    npm start script && hello world

---
+++ app/scripts/server/index.js
@@ -0,0 +1,10 @@
+const port = 8080;
+const app = require('express')();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+});
+
+app.listen(port, () => {
+	console.log(`Listening on ${port} port`);
+});
--- dist/js/server/index.js
+++ dist/js/server/index.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+
+var port = 8080;
+var app = require('express')();
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(port, function () {
+	console.log('Listening on ' + port + ' port');
+});
